7a3811bb64f7541f596694bcf9e46e6bede12945
Ananya yaa <2400032396@kluniversity.in>
2026-04-01 15:17:48 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "e-commerce-back-end-production-a05a.up.railway.app/api"
+  baseURL: "https://e-commerce-back-end-production-a05a.up.railway.app"
 });
 
 export default API;
